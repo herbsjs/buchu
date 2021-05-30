44fe0fa5e1c5313213ce4a29135559088212fcc7
Douglas Brito <douglas.f.brito@gmail.com>
2021-05-29 21:07:56 -0300

    add wasStopped conditional

--- src/step.js
+++ src/step.js
@@ -57,12 +57,15 @@ class Step {
                 step.context = this.context
 
                 let ret = await step.run()
-  
+
+                if (step.context._stopExecution) 
+                    step.auditTrail.wasStopped = true
+                
                 this._auditTrail.steps.push(step.auditTrail)
 
                 if (ret.isErr) return ret
 
-                if(step.context._stopExecution ) break
+                if(step.context._stopExecution) break
             }
             let ret = this.context.ret
             return Ok(ret)
