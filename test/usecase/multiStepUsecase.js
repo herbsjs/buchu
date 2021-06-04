8b7c7a58a980cae4f5308b69b5997ccf8f0608eb
Jhonathan S Soares <jx@vortx.com.br>
2021-06-04 10:57:23 -0300

    lint fix

--- test/usecase/multiStepUsecase.js
+++ test/usecase/multiStepUsecase.js
@@ -31,7 +31,7 @@ describe('A use case', () => {
                 //given
                 const uc = givenAMultiStepUseCase()
                 //when
-                const ret = await uc.run()
+                await uc.run()
                 //then
                 assert.deepStrictEqual(uc.auditTrail, {
                     type: 'use case',
@@ -72,7 +72,7 @@ describe('A use case', () => {
                 //given
                 const uc = givenAMultiStepUseCaseWithError()
                 //when
-                const ret = await uc.run()
+                await uc.run()
                 //then
                 assert.deepStrictEqual(uc.auditTrail, {
                     type: 'use case',
