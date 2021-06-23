8f4df08e823cf38645969bbe3b625ac19c018be5
vortx <italo.i@live.com>
2021-06-22 21:34:57 -0300

    :white_check_mark: added tests

--- src/results.js
+++ src/results.js
@@ -10,12 +10,13 @@ class Ok {
 	get isErr() { return false }
 	get ok() { return this.value }
 	get err() { return null }
+	
 	toString() {
 		if (this.value)
 			return 'Ok: ' + JSON.stringify(this.value)
-		else
-			return 'Ok'
+		return 'Ok'
 	}
+
 	toJSON() { return { 'Ok': (this.value === undefined ? '' : this.value) } }
 }
 
@@ -31,20 +32,65 @@ class Err {
 	get isErr() { return true }
 	get ok() { return null }
 	get err() { return this._error }
+
 	toString() {
 		if (this._error)
 			return "Error: " + JSON.stringify(this._error)
-		else
-			return "Error"
+		return "Error"
 	}
+
 	toJSON() {
 		let error = (this._error === undefined ? '' : this._error)
 		error = error instanceof Error ? error.toString() : error
 		return { 'Error': error }
 	}
+
+	static _notFound ({ message = 'Not Found', payload, stackTrace } = {}){ 
+		return _buildErr('NOT_FOUND', message, payload, stackTrace, 'NotFound', 'notFound')
+	}
+	
+	static _alreadyExists ({ message = 'Entity already exists', payload, stackTrace } = {}) { 
+		return _buildErr('ALREADY_EXISTS', message, payload, stackTrace, 'AlreadyExists')
+	}
+	
+	static _invalidEntity ({ message = 'Invalid entity', payload, stackTrace } = {}) { 
+		return _buildErr('INVALID_ENTITY', message, payload, stackTrace, 'InvalidEntity')
+	}
+	
+	static _invalidArguments ({ message = 'Invalid arguments', payload, stackTrace } = {}) { 
+		return _buildErr('INVALID_ARGUMENTS', message, payload, stackTrace, 'InvalidArguments')
+	}
+	
+	static _permissionDenied ({ message = 'Permission denied', payload, stackTrace } = {}) { 
+		return _buildErr('PERMISSION_DENIED', message, payload, stackTrace, 'PermissionDenied')
+	}
+	
+	static _unknown ({ message = 'Unknown Error', payload, stackTrace } = {}) { 
+		return _buildErr('UNKNOWN', message, payload, stackTrace, 'Unknown')
+	}
+	
+	static _unimplemented ({ message = 'Unimplemented', payload, stackTrace } = {}) { 
+		return _buildErr('UNIMPLEMENTED', message, payload, stackTrace, 'Unimplemented')
+	}
 }
 
-module.exports = {
-	Ok: (value) => new Ok(value),
-	Err: (err) => new Err(err)
+function _buildErr(code, message, payload, stackTrace, caller) {
+	const err = new Err({ payload, stackTrace, code, message })
+	err[`is${caller}Error`] = true
+	return err
 }
+
+const _exports = {
+	Ok: (value) => new Ok(value),
+	Err: (err) => new Err(err),
+}
+
+_exports.Err.notFound = Err._notFound
+_exports.Err.alreadyExists = Err._alreadyExists
+_exports.Err.invalidEntity  = Err._invalidEntity
+_exports.Err.invalidArguments = Err._invalidArguments
+_exports.Err.permissionDenied= Err._permissionDenied
+_exports.Err.unknown = Err._unknown
+_exports.Err.unimplemented = Err._unimplemented
+
+module.exports = _exports
---
+++ test/results/results.js
@@ -0,0 +1,288 @@
+const assert = require('assert')
+const { Err } = require('../../src/results')
+
+describe('Err',  () => {
+    it('should to return a notFound Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.notFound({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'NOT_FOUND',
+                    message: 'Not Found'
+                  }
+            },
+            {
+                ret: Err.notFound({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'NOT_FOUND',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.notFound(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'NOT_FOUND',
+                    message: 'Not Found'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isNotFoundError, true)
+        }
+        
+    })
+
+    it('should to return a alreadyExists Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.alreadyExists({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'ALREADY_EXISTS',
+                    message: 'Entity already exists'
+                  }
+            },
+            {
+                ret: Err.alreadyExists({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'ALREADY_EXISTS',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.alreadyExists(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'ALREADY_EXISTS',
+                    message: 'Entity already exists'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isAlreadyExistsError, true)
+        }
+        
+    })
+
+    it('should to return a invalidEntity Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.invalidEntity({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'INVALID_ENTITY',
+                    message: 'Invalid entity'
+                  }
+            },
+            {
+                ret: Err.invalidEntity({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'INVALID_ENTITY',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.invalidEntity(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'INVALID_ENTITY',
+                    message: 'Invalid entity'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isInvalidEntityError, true)
+        }
+        
+    })
+
+    it('should to return a invalidArguments Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.invalidArguments({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'INVALID_ARGUMENTS',
+                    message: 'Invalid arguments'
+                  }
+            },
+            {
+                ret: Err.invalidArguments({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'INVALID_ARGUMENTS',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.invalidArguments(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'INVALID_ARGUMENTS',
+                    message: 'Invalid arguments'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isInvalidArgumentsError, true)
+        }
+        
+    })
+
+    it('should to return a permissionDenied Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.permissionDenied({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'PERMISSION_DENIED',
+                    message: 'Permission denied'
+                  }
+            },
+            {
+                ret: Err.permissionDenied({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'PERMISSION_DENIED',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.permissionDenied(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'PERMISSION_DENIED',
+                    message: 'Permission denied'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isPermissionDeniedError, true)
+        }
+        
+    })
+
+    it('should to return a unknown Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.unknown({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'UNKNOWN',
+                    message: 'Unknown Error'
+                  }
+            },
+            {
+                ret: Err.unknown({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'UNKNOWN',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.unknown(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'UNKNOWN',
+                    message: 'Unknown Error'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isUnknownError, true)
+        }
+        
+    })
+
+    it('should to return a unimplemented Err',  () => {
+        // given
+        const scenarios = [
+            {
+                ret: Err.unimplemented({payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'UNIMPLEMENTED',
+                    message: 'Unimplemented'
+                }
+            },
+            {
+                ret: Err.unimplemented({ message: 'message', payload: { entity: 'user' }, stackTrace: {foo: 'bar'} }),
+                expect: {
+                    payload: { entity: 'user' },
+                    stackTrace: {foo: 'bar'},
+                    code: 'UNIMPLEMENTED',
+                    message: 'message'
+                  }
+            },
+            {
+                ret: Err.unimplemented(),
+                expect: {
+                    payload: undefined,
+                    stackTrace: undefined,
+                    code: 'UNIMPLEMENTED',
+                    message: 'Unimplemented'
+                  }
+            }
+        ]
+
+        // then
+        for(const scenario of scenarios) {
+            assert.deepStrictEqual(scenario.ret.err, scenario.expect)
+            assert.deepStrictEqual(scenario.ret.isUnimplementedError, true)
+        }
+        
+    })
+
+
+
+  
+})
